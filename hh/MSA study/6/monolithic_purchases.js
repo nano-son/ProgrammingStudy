2594748984822a96207a2fd694fe4346c7b520d0
hh1712.jeon <hh1712.jeon@samsung.com>
2018-05-29 10:44:31 +0900

    모놀리식 테스트코드 작성

---
+++ hh/MSA study/6/monolithic_purchases.js
@@ -0,0 +1,111 @@
+const mysql = require('mysql');
+const conn = {
+    host: 'localhost',
+    user: 'micro',
+    password: 'service',
+    database: 'monolithic'
+};
+
+exports.onRequest = function (res, method, pathname, params, cb) {
+    
+        switch (method) {
+            case "POST":
+                return register(method, pathname, params, (response) => {
+                    process.nextTick(cb, res, response);
+                });
+            case "GET":
+                return inquiry(method, pathname, params, (response) => {
+                    process.nextTick(cb, res, response);
+                });
+            default:
+                return process.nextTick(cb, res, null);
+        }
+}
+
+function register(method, pathname, params, cb) {
+
+    var response = {
+        key: params.key,
+        errorcode: 0,
+        errormessage: "success"
+    }
+
+    if (params.userid == null || params.goodsid == null) {
+        response.errorcode = 1;
+        response.errormessage = "Invalid Parameters";
+        cb(response);
+    } else {
+        var connection = mysql.createConnection(conn);
+        connection.connect();
+        connection.query("insert into purchases(userid, goodsid) values(?,?)"
+                        , [params.userid, params.goodsid]
+                        , (error, results, fields) => {
+                            if(error) {
+                                response.errorcode = 1;
+                                reesponse.errormessage = error;
+                            }
+                            cb(response);
+                        });
+        connection.end();
+    }
+}
+
+
+function inquiry(method, pathname, params, cb) {
+
+    var response = {
+        key: params.key,
+        errorcode: 0,
+        errormessage: "success"
+    };
+
+    if(params.userid == null) {
+        response.errorcode = 1;
+        response.errormessage = "Invalid Parameters";
+        cb(response);
+    } else {
+        var connection = mysql.createConnection(conn);
+        connection.connect();
+        connection.query("select id, goodsid, date from purchases where userid = ?"
+                        , [params.userid]
+                        , (error, results, fields) => {
+                            if (error) {
+                                response.errorcode = 1;
+                                response.errormessage = error;
+                            } else {
+                                response.results = results;
+                            }
+                            cb(response);
+                        });
+        connection.end();
+    }
+}
+
+function unregister(method, pathname, params, cb) {
+
+    var response = {
+        key: params.key,
+        errorcode: 0,
+        errormessage: "success"
+    };
+
+    if (params.id == null) {
+        response.errorcode = 1;
+        response.errormessage = "Invalid Parameters";
+        cb(response);
+    } else {
+
+        var connection = mysql.createConnection(conn);
+        connection.connect();
+        connection.query("delete from goods where id=?"
+                         , [params.id]
+                        , (error, results, fields) => {
+                            if (error) {
+                                response.errorcode = 1;
+                                response.errormessage = error;
+                            }
+                            cb(response);
+                        });
+        connection.end();
+    }
+}
